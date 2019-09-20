f31735525e4a0250c3ec51035525422f696bb225
Sasha Kolachko <kolachko999@gmail.com>
2019-09-20 10:44:54 +0300

    header with search input done

---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template lang="html">
+  <span>Vuetify</span>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template lang="html">
+  <v-app-bar height="84" app class="align-center">
+      <v-row class="align-center">
+        <v-col cols="12" sm="3" md="3" lg="1">
+          <v-img
+          src="logo1.png"
+           max-height="80"
+           max-width="80"
+          >
+          </v-img>
+        </v-col>
+        <v-col cols="12" sm="3" md="3" lg="4">
+          <v-text-field
+          clearable
+          color="secondary"
+          placeholder="What would you like to cook?"
+          prepend-icon="search"
+          >
+          </v-text-field>
+        </v-col>
+        <v-col cols="12" sm="3" md="3" lg="4">
+          <v-btn color="primary">Search</v-btn>
+        </v-col>
+     </v-row>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="css" scoped>
+</style>
